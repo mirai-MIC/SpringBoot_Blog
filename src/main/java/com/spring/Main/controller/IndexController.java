dc0435f2f2c30e43df2303d378f5f2f3891ebd7a
mirai-MIC <3092179918@qq.com>
2022-10-05 21:09:10 +0800

    此项目无用，留作测试

---
+++ src/main/java/com/spring/Main/controller/IndexController.java
@@ -0,0 +1,8 @@
+package com.spring.Main.controller;/**
+* Copyright (C), 2022-10-05
+* FileName: IndexController
+* Author:   mirai
+* Date:     2022/10/5 18:02
+* Description: 
+*/public class IndexController {
+}
