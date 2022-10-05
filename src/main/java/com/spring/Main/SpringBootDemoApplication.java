180748f511d847bcbebd0bd9d07ff7c708fe73b6
mirai-MIC <3092179918@qq.com>
2022-10-05 21:09:10 +0800

    此项目无用，留作测试

---
+++ src/main/java/com/spring/Main/SpringBootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.spring.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mirai
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
--- src/main/java/com/spring/Main/controller/IndexController.java
+++ src/main/java/com/spring/Main/controller/IndexController.java
@@ -1,8 +1,22 @@
-package com.spring.Main.controller;/**
-* Copyright (C), 2022-10-05
-* FileName: IndexController
-* Author:   mirai
-* Date:     2022/10/5 18:02
-* Description: 
-*/public class IndexController {
+package com.spring.Main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright (C), 2022-10-05
+ * FileName: IndexController
+ * Author:   mirai
+ * Date:     2022/10/5 18:02
+ * Description:
+ * @author mirai
+ */
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = "index")
+    public String getIndex() {
+        return "index";
+    }
 }
